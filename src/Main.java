98fef9e7d0801d2b9d14501b89620f172a14566f
tiopw <habibamuhammed202@gmail.com>
2025-09-16 15:58:37 +0000

    added an interactive quiz game

--- src/Main.java
+++ src/Main.java
@@ -221,7 +221,7 @@ public class Main {
         }
 */
 
-        char[][] telephone = {{'1','2','3'},
+       /* char[][] telephone = {{'1','2','3'},
                               {'4','5','6'},
                               {'7','8','9'},
                               {'*','0','#'}
@@ -233,6 +233,57 @@ public class Main {
             }
             System.out.println();
         }
+        */
+
+        //java quiz game
+
+        String[] questions = {  "what is the function of a router?",
+                                "what part of the computer is considered the brain?",
+                                "what year was facebook launched?",
+                                "who is known as the father of computers?",
+                                "what was the first programming language?"
+        };
+
+        String[][] options = {  {"1. storing files", "2. encrypting data", "3. directing internet traffic"},
+                                {"1. cpu", "2. hard drive", "3. ram"},
+                                {"1. 2000", "2. 2005", "3. 2004"},
+                                {"1. steve jobs", "2. charles babbage", "3. alan turing"},
+                                {"1. java", "2.c", "3. fortran"}
+        };
+
+        int[] answers = {3, 1, 3, 2, 3};
+        int score = 0;
+        int guess;
+
+        System.out.println("************************");
+        System.out.println("welcome to the quiz game");
+        System.out.println("************************");
+
+        for (int i = 0; i < questions.length; i++){
+
+            System.out.println(questions[i]);
+            for (String option : options[i]){
+                System.out.println(option);
+            }
+            System.out.print("enter your guess: ");
+            guess = scanner.nextInt();
+
+            if (guess == answers[i]){
+                System.out.println("********");
+                System.out.println("correct!");
+                System.out.println("*********");
+                score++;
+            }
+            else{System.out.println("********");
+                System.out.println("wrong! ");
+                System.out.println("*********");
+
+            }
+
+        }
+        System.out.println("your final score is: " + score + " out of " + questions.length);
+
+
         scanner.close();
     }
     /* static void showBalance(double balance){
